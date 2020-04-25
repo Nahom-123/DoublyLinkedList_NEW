9ea59e764c8534998a3f81b83cefc1f023b3da91
Nahom-123 <nahomdagnewnegash@gmail.com>
2020-04-25 18:18:11 -0400

    Name

--- DoublyLinkedList/main.cpp
+++ DoublyLinkedList/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     //std::cout << "Hello, World!\n";
     DLinkedList first;
-    
+    cout << "Nahom Dagnew";
     first.addFront("Back");
     first.addFront("Middle");
     first.addFront("Front");
